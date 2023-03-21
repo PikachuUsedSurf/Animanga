bf141ba3887ca61d351e449d998faca59a819dc2
PikachuUsedSurf <nanaosafokissi@gmail.com>
2023-03-20 23:40:36 -0700

    the navbar update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import useCurrentUser from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next";
-import { getSession, signOut } from "next-auth/react";
+import { getSession} from "next-auth/react";
+import Navbar from "@/components/Navbar";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -20,12 +21,9 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
   return (
     <>
-      <h1 className="text-lg text-green-500">welcome</h1>
-      <p className="text-white">login as : {user?.name}</p>
-      <button onClick={() => signOut()} className="h-10 w-24 bg-slate-600">logout</button>
+      <Navbar/>
     </>
   )
 }
