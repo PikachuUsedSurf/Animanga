7f94f377f90959c36dc7bd386839c167b36a8de9
PikachuUsedSurf <nanaosafokissi@gmail.com>
2023-03-23 14:25:28 +0300

    FINALLY DONE!!!

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BsFillPlayCircleFill } from 'react-icons/bs';
 import FavoriteButton from './FavoriteButton';
+import router, { useRouter } from 'next/router';
+import useInfoModal from '@/hooks/useInfoModal';
+import { BiChevronDown } from 'react-icons/bi';
 
 interface MovieCardProps {
     data: Record<string, any>;
@@ -9,6 +12,9 @@ interface MovieCardProps {
 const MovieCard: React.FC<MovieCardProps> = ({
     data
 }) => {
+
+  const router = useRouter();
+  const { openModal } = useInfoModal();
     return(
         <div className='group bg-zinc-900 col-span relative h-[12vw]'>
           <img src={data.thumbnailUrl} alt="Movie" draggable={false} className="
@@ -76,10 +82,26 @@ const MovieCard: React.FC<MovieCardProps> = ({
                 items-center
                 transition
                 hover:bg-neutral-300
-                ' onClick={() => {}}>
+                ' onClick={() => router.push(`/watch/${data?.id}`)}>
                     <BsFillPlayCircleFill size={30} />
                 </div>
                 <FavoriteButton movieId={data?.id} />
+                <div 
+                onClick={() => openModal(data?.id)}
+                className='
+                cursor-pointer 
+                ml-auto 
+                group/item 
+                w-6 h-6 
+                lg:h-10 lg:w-10 
+                border-white border-2 rounded-full 
+                flex justify-center items-center 
+                transition hover:border-neutral-300
+                '>
+                  <BiChevronDown  
+                  size={30}
+                  className='text-white group-hover/items:text-neutral-300'/>
+                </div>
             </div>
               <p className="text-green-400 font-semibold mt-4">
                 New <span className="text-white">2023</span>
---
+++ components/PlayButton.tsx
@@ -0,0 +1,34 @@
+import react from 'react';
+import { BsFillPlayFill } from 'react-icons/bs';
+import { useRouter } from 'next/router';
+
+interface PlayButtonProps {
+    movieid: string;
+}
+
+const PlayButton: React.FC<PlayButtonProps> = ({ movieid }) => {
+    const router = useRouter();
+    
+    return(
+        <button 
+        onClick={() => router.push(`/watch/${movieid}`)}
+        className='
+        bg-white
+        rounded-md
+        py-1 md:py-2
+        px-2 md:px-4
+        w-auto
+        text-xs lg:text-lg
+        font-semibold
+        flex flex-row
+        items-center
+        hover:bg-neutral-300
+        transition
+        '>
+        <BsFillPlayFill size={25} className='mr-1' />
+        Play
+        </button>
+    )
+};
+
+export default PlayButton;
